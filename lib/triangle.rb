bd7ea4ea87dca5c111462349eefc536279a929bc
evan <ejeffries14@gmail.com>
2021-07-05 23:45:57 -0500

    commit tree deb03d4069bfc089ba60b7484f2cfb29f6539e0c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def inequality
+    sides = [length_1, length_2, length_3]
+    sides.any? {|side| side <= 0 } || sides[0] + sides[1] <= sides[2]
+  end
+
+  def kind
+    if inequality
+      raise TriangleError
+    elsif length_1 + length_2 <= length_3 || length_2 + length_3 <= length_1 || length_1 + length_3 <= length_2
+      raise TriangleError
+    elsif length_1 == length_2 && length_1 == length_3
+      :equilateral
+    elsif length_1 == length_2 || length_1 == length_3 || length_2 == length_3
+      :isosceles
+    elsif length_1 != length_2 && length_3
+      :scalene
+    end
+  end
+    
+  class TriangleError < StandardError
+  
+  end
 end
